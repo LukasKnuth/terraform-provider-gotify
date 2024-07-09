04e678530ebb53ef2bd8a228ff8e65d5d6cfad96
Lukas Knuth <me@lknuth.dev>
2024-07-09 20:02:38 +0200

    Tests for `gotify_client` resource.
    This is basically copied from the `gotify_application` resource tests. Neither currently work because they try to make actual HTTP requests. Looking into that...

--- client_resource_test.go
+++ client_resource_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestApplicationResource(t *testing.T) {
+func TestClientResource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -14,14 +14,12 @@ func TestApplicationResource(t *testing.T) {
 			// Test Create() and Read()
 			{
 				Config: providerConfig + `
-resource "gotify_application" "test" {
+resource "gotify_client" "test" {
  name = "Testing"
- description = "Test description"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("gotify_application.test", "name", "Testing"),
-					resource.TestCheckResourceAttr("gotify_application.test", "description", "Test description"),
 					resource.TestCheckResourceAttrSet("gotify_application.test", "id"),
 					resource.TestCheckResourceAttrSet("gotify_application.test", "token"),
 				),
@@ -29,14 +27,12 @@ resource "gotify_application" "test" {
 			// Test Update() and Read()
 			{
 				Config: providerConfig + `
-resource "gotify_application" "test" {
+resource "gotify_client" "test" {
  name = "Changed"
- description = "Changed description"
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("gotify_application.test", "name", "Changed"),
-					resource.TestCheckResourceAttr("gotify_application.test", "description", "Changed description"),
 				),
 			},
 		},

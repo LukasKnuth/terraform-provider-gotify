567eb2e6e0dd8f88af4291e09f0fd8e3d7fadb5a
Lukas Knuth <me@lknuth.dev>
2024-07-10 15:16:15 +0200

    Updated provider address to final Terraform Registry address.
    Not sure if this is whats preventing the provider from being published on the registry.

--- main.go
+++ main.go
@@ -32,10 +32,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/hashicorp/gotify",
+		Address: "registry.terraform.io/LukasKnuth/gotify",
 		Debug:   debug,
 	}
 

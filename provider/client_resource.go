437b1b829a5f207843c165277428bc1cdaa69678
Troy C. <327529+troxor@users.noreply.github.com>
2026-08-08 16:39:22 -0500

    Persist application tokens in state

--- provider/client_resource.go
+++ provider/client_resource.go
@@ -136,7 +136,9 @@ func (r *ClientResource) Read(ctx context.Context, req resource.ReadRequest, res
 	if found != nil {
 		// Update information on state
 		state.Name = types.StringValue(found.Name)
-		state.Token = types.StringValue(found.Token)
+		if found.Token != "" {
+			state.Token = types.StringValue(found.Token)
+		}
 
 		// Write new information to tf-state
 		resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
